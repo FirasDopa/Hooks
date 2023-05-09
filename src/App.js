412f1a4b43786361dc3d76c7f2c8b72631c61f63
FirasDopa <mohamedfiras.silini@esprit.tn>
2023-05-09 19:54:17 +0100

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import NavBars from './components/Navbars/NavBars';
+import {moviesData} from './components/Data/Data';
+import ListMovie from './components/ListMovies/ListMovie';
+import FilterMovies from './components/FilterMovies/FilterMovies';
+import AddMovie from './components/AddMovie/AddMovie';
+import Carousls from './components/Carousels/Carousls';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [movies, SetMovies] = useState(moviesData)
+
+    const [inputSearch, setInputSearch] = useState("")
+    const [rateSearch, setRateSearch] = useState(0)
+
+    console.log(rateSearch)
+    const add = (newMovie) => {
+        SetMovies([
+            ...movies,
+            newMovie
+        ])
+    }
+
+
+    return (
+        <div className="App">
+            <NavBars/>
+            <Carousls/>
+            <FilterMovies inputSearch={inputSearch}
+                setInputSearch={setInputSearch}
+                rateSearch={rateSearch}
+                setRateSearch={setRateSearch}/>
+            <AddMovie add={add}/>
+            <ListMovie movies={movies}
+                inputSearch={inputSearch}
+                rateSearch={rateSearch}/>
+
+
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/components/Data/Data.js
@@ -0,0 +1,77 @@
+export const moviesData = [
+    {
+        id: 1,
+        title: "RUST PRO'S by HJUNE",
+        rate: 4,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/2fQxrxoqKJg/maxresdefault.jpg",
+        trailer: "https://www.youtube.com/watch?v=2fQxrxoqKJg&t=58s&ab_channel=hJune",
+    }
+    ,
+    {
+        id: 2,
+        title: "THE GREATEST RAID DEFENSE by HJUNE ",
+        rate: 5,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/Blb3bmmHnWY/sddefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=Blb3bmmHnWY&t=584s&ab_channel=hJune",
+    }
+    ,
+    {
+        id: 3,
+        title: "CHASING THE AK GHOST by HJUNE",
+        rate: 4,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/_z_uZMpm80Y/mqdefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=_z_uZMpm80Y&ab_channel=hJune",
+        }
+    ,
+    {
+        id: 4,
+        title: "WHAT 30000 hours of rust looks like by HJUNE ",
+        rate: 5,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/PuP7EIxPH4c/maxresdefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=PuP7EIxPH4c&ab_channel=hJune",
+    }
+    ,
+    {
+        id: 5,
+        title: "OBLITERATION by Blooprint",
+        rate: 3,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/F8v1S51oiq8/maxresdefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=F8v1S51oiq8&ab_channel=Blooprint",
+    },
+    {
+        id: 6,
+        title: "DUO DANGER by Blooprint",
+        rate: 5,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/dEjtf4euq1g/maxresdefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=dEjtf4euq1g&ab_channel=Blooprint",
+    },
+    {
+        id: 7,
+        title: "A DUO SEEKS REVENGE by Blooprint",
+        rate: 4,
+        description:
+            "The only aim in Rust is to survive. Everything wants you to die - the island's wildlife and other inhabitants, the environment, other survivors.",
+        posterUrl:
+            "https://i.ytimg.com/vi/0QoW14KZ64Q/maxresdefault.jpg",
+        trailer:"https://www.youtube.com/watch?v=6HDkkS6l1dw&t=221s&ab_channel=Blooprint",
+    },
+    
+    ];
